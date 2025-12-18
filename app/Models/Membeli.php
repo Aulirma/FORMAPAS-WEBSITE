961f7662d3d36a5f105cc1a6c796fd2fe7b0112b
Aulirma <24051204005@mhs.unesa.ac.id>
2025-12-19 06:39:47 +0700

    Add 'alasan_penolakan' to Membeli model attributes

--- app/Models/Membeli.php
+++ app/Models/Membeli.php
@@ -20,6 +20,7 @@ class Membeli extends Model
         "alamat_pengiriman",  
         "no_wa",             
         "status",
+        "alasan_penolakan",
         "created_at",
         "updated_at"       
     ];
